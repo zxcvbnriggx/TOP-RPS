51ae3c1ff324743bee62c0034cca751f447330b2
Mark Reagan <171141812+zxcvbnriggx@users.noreply.github.com>
2025-12-12 05:36:28 +0800

    feat: add getHumanChoice function

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function getComputerChoice() {
 	const index = Math.floor(Math.random() * choices.length);
 	return choices[index];
 }
+
+function getHumanChoice() {
+	while (true) {
+		const input = window.prompt('Enter rock, paper, or scissor: ');
+
+		if (input === null) return null;
+
+		const choice = input.trim().toLowerCase();
+
+		const isInputValid = choices.includes(choice);
+
+		if (isInputValid) return choice;
+	}
+}
+
+console.log(getHumanChoice());
