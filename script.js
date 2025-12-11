e52eda2386ae6cb2708eee94f2f4b7d74dd5bac1
Mark Reagan <171141812+zxcvbnriggx@users.noreply.github.com>
2025-12-12 07:04:47 +0800

    feat: implement play game logic

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const choices = ['rock', 'paper', 'scissor'];
-let humanScore = 0;
-let computerScore = 0;
 
 function getComputerChoice() {
 	const index = Math.floor(Math.random() * choices.length);
@@ -21,28 +19,49 @@ function getHumanChoice() {
 	}
 }
 
-function playRound(humanChoice, computerChoice) {
-	if (!humanChoice) return 'Game cancelled';
+function playGame() {
+	let humanScore = 0;
+	let computerScore = 0;
+	let round = 0;
 
-	if (humanChoice === computerChoice)
-		return `Tie!, you both chose ${humanChoice}`;
+	function playRound(humanChoice, computerChoice) {
+		if (!humanChoice) {
+			round = 99;
+			return 'Game cancelled';
+		}
 
-	const winAgainst = {
-		rock: 'scissor',
-		paper: 'rock',
-		scissor: 'paper',
-	};
+		if (humanChoice === computerChoice)
+			return `Tie!, you both chose ${humanChoice}`;
 
-	if (winAgainst[humanChoice] === computerChoice) {
-		humanScore++;
-		return 'You win the round';
+		const winAgainst = {
+			rock: 'scissor',
+			paper: 'rock',
+			scissor: 'paper',
+		};
+
+		if (winAgainst[humanChoice] === computerChoice) {
+			humanScore++;
+			return 'You win the round';
+		}
+
+		computerScore++;
+		return 'You lose the round';
 	}
 
-	computerScore++;
-	return 'You lose the round';
+	while (round < 5) {
+		console.log(`Round ${round + 1}`);
+
+		const humanSelection = getHumanChoice();
+		const computerSelection = getComputerChoice();
+
+		console.log(playRound(humanSelection, computerSelection));
+
+		round++;
+	}
+
+	console.log('--- Game over ---');
+	console.log(`Your score: ${humanScore}`);
+	console.log(`Computer score: ${computerScore}`);
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-console.log(playRound(humanSelection, computerSelection));
+playGame();
