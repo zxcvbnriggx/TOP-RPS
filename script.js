5e73da967564a6b517b7f0dc619fabaf9931f8e1
Mark Reagan <171141812+zxcvbnriggx@users.noreply.github.com>
2025-12-12 05:10:30 +0800

    feat: implent random computer move generator

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello, world!');
+const choices = ['rock', 'paper', 'scissor'];
+
+function getComputerChoice() {
+	const index = Math.floor(Math.random() * choices.length);
+	return choices[index];
+}
